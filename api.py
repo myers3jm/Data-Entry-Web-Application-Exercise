c54e0e6ea9f5f6ebf7c11db740569bcb5f63f27e
Jared Myers <jarmyrs@gmail.com>
2025-08-07 16:30:18 -0400

    API - Add routes for entry, submission, and data viewing
    This change adds routes within Flask to show the data entry page, sent the POST request with the entered data, and view all data in the database

--- api.py
+++ api.py
@@ -1,12 +1,13 @@
 # Author:   Jared Myers
 # Date:     08.07.2025
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
+# Model of how data should be stored in the database
 class DataModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -17,11 +18,35 @@ class DataModel(db.Model):
     def __repr__(self):
         return f'Data(name={self.name}, age={self.age}, title={self.title}, hometown={self.hometown})'
 
+# Web interface routes
 @app.route('/')
 def home():
     return '''
     <h1>Data Entry Web Application</h1>
-    <p>Created by Jared Myers, 08.07.2025</p>'''
+    <p>Created by Jared Myers, 08.07.2025</p>
+    <a href="/entry/">Begin entering data</a>'''
 
+@app.route('/entry/')
+def entry():
+    return render_template('entry.html')
+
+@app.route('/submit/', methods=['POST'])
+def submit_form():
+    name = request.form['name']
+    age = request.form['age']
+    title = request.form['title']
+    hometown = request.form['hometown']
+
+    data = DataModel(name=name, age=age, title=title, hometown=hometown)
+    db.session.add(data)
+    db.session.commit()
+
+    return redirect('/data/')
+
+@app.route('/data/')
+def view_data():
+    return render_template('data.html', data=DataModel.query.all())
+
+# Main program
 if __name__ == '__main__':
     app.run(debug=True)
